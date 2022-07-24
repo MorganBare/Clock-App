c53c9f325c98109a1673efcafe95d0da1bc2fd8f
MorganBare <morganjbare@gmail.com>
2022-07-24 14:28:39 +0200

    refactor - deleted some css files that were not used, added a index.scss file as the main file that everything will be imported to, Style - starting the base styles

---
+++ src/components/Test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../scss/test.scss';
+
+export default function Test(){
+    return (
+        <div className='test'>
+            <p>shdfgsdjhfgdsjhfgds</p>
+        </div>
+    )
+}
