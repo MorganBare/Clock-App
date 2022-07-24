6c32c3a97b1da1fc232b2f9d2cf6868c9ee9589c
MorganBare <morganjbare@gmail.com>
2022-07-24 16:10:03 +0200

    Programming quote API has been added and provides a new quote on page refresh. Removed React strict mode fromt index.js as this was rendering the Quote component twice

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,13 +1,25 @@
 import refresh from '../assets/desktop/icon-refresh.svg';
+import React from 'react';
 
 export default function Quote(){
+
+    const [ProgrammingQuote, setQuote] = React.useState({});
+
+    React.useEffect(() => {
+        fetch('https://programming-quotes-api.herokuapp.com/Quotes/random')
+        .then(res => res.json())
+        .then(data => setQuote(data))
+    }, [])
+
+    console.log(ProgrammingQuote)
+
     return (
         <div className="fs-3">
             <div>
-                <p>Quote would go here</p>
+                <p>{ProgrammingQuote.en}</p>
                 <img src={refresh} alt='refresh icon'/>
             </div>
-            <p>Author</p>
+            <p>{ProgrammingQuote.author}</p>
         </div>
     )
 }
