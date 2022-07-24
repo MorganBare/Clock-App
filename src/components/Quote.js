f50499710bb379bb85c99fbdfc122c91bb15485f
MorganBare <morganjbare@gmail.com>
2022-07-24 16:37:29 +0200

    Feature - Added Axios and refactored the Quote component with the API so the API is called each time the refresh icon is clicked or the page refreshes

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,23 +1,39 @@
 import refresh from '../assets/desktop/icon-refresh.svg';
 import React from 'react';
+import axios from "axios";
 
 export default function Quote(){
 
     const [ProgrammingQuote, setQuote] = React.useState({});
 
-    React.useEffect(() => {
-        fetch('https://programming-quotes-api.herokuapp.com/Quotes/random')
-        .then(res => res.json())
-        .then(data => setQuote(data))
-    }, [])
+    const fetchQuote = async () => {
+        try{
+          const response = await axios('https://programming-quotes-api.herokuapp.com/Quotes/random');
+          setQuote(response.data);
+          console.log(response.data)
+        }catch(err){
+          console.error(err)
+        }
+      };
 
-    console.log(ProgrammingQuote)
+    React.useEffect(() => {
+        fetchQuote();
+        }, []);
+
+    function getNewQuote(){
+        fetchQuote()
+        setQuote(ProgrammingQuote)
+    }
 
     return (
         <div className="fs-3">
             <div>
                 <p>{ProgrammingQuote.en}</p>
-                <img src={refresh} alt='refresh icon'/>
+                <img 
+                src={refresh} 
+                alt='refresh icon'
+                onClick={getNewQuote}
+                />
             </div>
             <p>{ProgrammingQuote.author}</p>
         </div>
