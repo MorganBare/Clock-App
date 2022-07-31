0879a46682e3ecf472398c130c3cb3ead34748eb
MorganBare <morganjbare@gmail.com>
2022-07-31 11:38:35 +0200

    Added a function to handle the correct greeting depending on the time of day

--- src/components/FullPage.js
+++ src/components/FullPage.js
@@ -8,6 +8,16 @@ export default function FullPage(props){
 
     const worldTime = props.worldTime.datetime;
     const splitTime = worldTime.split(':');
+
+    function greeting(){
+        if(splitTime[0].slice(-2) <= 11){
+            return 'Good Morning'
+        } else if(splitTime[0].slice(-2) >= 12 && splitTime[0].slice(-2) <= 18){
+            return 'Good Afternoon'
+        } else {
+            return 'Good Evening'
+        }
+    };
     
     return (
         <div>
@@ -15,13 +25,14 @@ export default function FullPage(props){
             {!props.displayMoreInfo && <Quote />}
             <div>
                 <div className='upper'>
-                    <p className='font-size-4'><span><img src={Sun} alt='sun icon'/></span>Good Morning, it's currently</p>
+                    <p className='font-size-4'><span><img src={Sun} alt='sun icon'/></span>{greeting()}, it's currently</p>
                     <p className='font-size-8'>{splitTime[0].slice(-2)}:{splitTime[1]}<span className='font-size-6'>BST</span></p>
                     <p className='font-size-5'>in {props.city}, {props.country_code}</p>
                 </div>
                 <button onClick={props.handleDisplay} 
-                className='font-size-2 upper'>
-                    {props.displayMoreInfo ? <p>Less<span><img src={UpArrow} alt='up arrow'/></span></p> : <p>More<span><img src={DownArrow} alt='down arrow'/></span></p> }</button>
+                    className='font-size-2 upper'>
+                    {props.displayMoreInfo ? <p>Less<span><img src={UpArrow} alt='up arrow'/></span></p> : <p>More<span><img src={DownArrow} alt='down arrow'/></span></p> }
+                </button>
             </div>
         </div>
     )
