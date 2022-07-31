2caeec5f28c7ed3e18f0c2b21fc557e51cf2b443
MorganBare <morganjbare@gmail.com>
2022-07-31 11:44:33 +0200

    Added ternary to toggle sun and moon icon based on the time

--- src/components/FullPage.js
+++ src/components/FullPage.js
@@ -1,5 +1,6 @@
 import Quote from './Quote.js';
 import Sun from '../assets/desktop/icon-sun.svg';
+import Moon from '../assets/desktop/icon-moon.svg';
 import DownArrow from '../assets/desktop/icon-arrow-down.svg';
 import UpArrow from '../assets/desktop/icon-arrow-up.svg';
 import React from 'react';
@@ -12,7 +13,7 @@ export default function FullPage(props){
     function greeting(){
         if(splitTime[0].slice(-2) <= 11){
             return 'Good Morning'
-        } else if(splitTime[0].slice(-2) >= 12 && splitTime[0].slice(-2) <= 18){
+        } else if(splitTime[0].slice(-2) >= 12 && splitTime[0].slice(-2) <= 17){
             return 'Good Afternoon'
         } else {
             return 'Good Evening'
@@ -25,7 +26,7 @@ export default function FullPage(props){
             {!props.displayMoreInfo && <Quote />}
             <div>
                 <div className='upper'>
-                    <p className='font-size-4'><span><img src={Sun} alt='sun icon'/></span>{greeting()}, it's currently</p>
+                    <p className='font-size-4'>{splitTime[0].slice(-2) <= 18 && splitTime[0].slice(-2) > 7 ? <span><img src={Sun} alt='sun icon'/></span> : <span><img src={Moon} alt='moon icon'/></span>}{greeting()}, it's currently</p>
                     <p className='font-size-8'>{splitTime[0].slice(-2)}:{splitTime[1]}<span className='font-size-6'>BST</span></p>
                     <p className='font-size-5'>in {props.city}, {props.country_code}</p>
                 </div>
